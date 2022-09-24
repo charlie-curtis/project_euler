68d7d923f3282e51ac7034fa891336d7574213a1
Charlie Curtis <charlie-curtis@outlook.com>
2022-09-24 13:14:03 -0400

    add problem2

---
+++ project_euler/src/problem_2/Problem2.java
@@ -0,0 +1,31 @@
+package problem_2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Problem2 {
+
+  public static final int CUTOFF_VALUE = 4_000_000;
+  public static void main(String [] args) {
+
+    System.out.printf("\nThe sum of the even terms in fib less than %d is %d\n", CUTOFF_VALUE, evenTermFib());
+  }
+
+  public static int evenTermFib() {
+
+    int previousTerm = 0;
+    int currentTerm = 1;
+
+    int evenSum = 0;
+    while (currentTerm < CUTOFF_VALUE) {
+      if (currentTerm % 2 == 0) {
+        evenSum+= currentTerm;
+      }
+      int temp = currentTerm;
+      currentTerm = currentTerm + previousTerm;
+      previousTerm = temp;
+    }
+
+    return evenSum;
+  }
+}
