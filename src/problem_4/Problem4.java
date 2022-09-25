dd55d81a379fe325ec2f70cb4179d106824db672
Charlie Curtis <charlie-curtis@outlook.com>
2022-09-24 21:42:19 -0400

    do problem 4

---
+++ src/problem_4/Problem4.java
@@ -0,0 +1,36 @@
+package problem_4;
+
+public class Problem4 {
+
+  public static void main(String [] args) {
+
+    //tried 580085. It wasn't right
+    System.out.printf("\nThe largest palindrome product is %d\n", getPalindromeProduct());
+  }
+
+  public static long getPalindromeProduct() {
+    long rangeBegin = 100;
+    long rangeEnd = 999;
+
+    long max = 0;
+    for (long i = rangeEnd; i >= rangeBegin; i--) {
+      for (long j = rangeEnd; j >= rangeBegin; j--) {
+        if (isPalindrome(i*j)) {
+          max = Math.max(max, i*j);
+        }
+      }
+    }
+
+    return max;
+  }
+
+  public static boolean isPalindrome(long value) {
+    long sum = 0;
+    long originalValue = value;
+    while (value > 0) {
+      sum = sum * 10 + value % 10;
+      value = value / 10;
+    }
+    return sum == originalValue;
+  }
+}
