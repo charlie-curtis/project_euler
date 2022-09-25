e8f688b4b1e9723efb625131d58eb16cfad5d4ea
Charlie Curtis <charlie-curtis@outlook.com>
2022-09-24 23:58:10 -0400

    do problem 6

---
+++ src/Problem_6/Problem6.java
@@ -0,0 +1,22 @@
+package Problem_6;
+
+import java.util.HashMap;
+import java.util.stream.LongStream;
+
+public class Problem6 {
+
+  public static void main(String[] args) {
+
+    System.out.printf("\nThe answer is %d\n", compute());
+  }
+
+  public static long compute() {
+    long endRange = 100;
+
+    long squareOfSums = (long) Math.pow(LongStream.range(1, endRange + 1).sum(), 2);
+    long sumOfSquares = LongStream.range(1, endRange + 1).map(val -> val*val).sum();
+
+    return Math.abs(sumOfSquares - squareOfSums);
+  }
+
+}
