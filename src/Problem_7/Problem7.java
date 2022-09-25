4380ca759f8ad81e05a4c3ecaca0285e649825fa
Charlie Curtis <charlie-curtis@outlook.com>
2022-09-25 00:22:33 -0400

    do problem 7

---
+++ src/Problem_7/Problem7.java
@@ -0,0 +1,35 @@
+package Problem_7;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Problem7 {
+
+  public static void main(String[] args) {
+
+    System.out.printf("\nThe answer is %d\n", compute());
+  }
+
+  public static long compute() {
+    int cutoff = 10_001;
+
+    ArrayList<Integer> primes = new ArrayList<>();
+    int i = 2;
+    while (primes.size() <= cutoff) {
+      boolean isPrime = true;
+      for (int j = 0; j < primes.size(); j++) {
+        if (i % primes.get(j) == 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime) {
+        primes.add(i);
+      }
+      i++;
+    }
+
+    return primes.get(cutoff-1);
+  }
+
+}
