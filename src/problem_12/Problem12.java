7ca299253d631bad3b87c41edef925c2bed2e88a
Charlie Curtis <charlie-curtis@outlook.com>
2022-09-25 21:52:52 -0400

    do problem 12

--- src/problem_12/Problem12.java
+++ src/problem_12/Problem12.java
@@ -8,6 +8,31 @@ public class Problem12 {
   }
 
   public static long compute() {
-    return 0;
+    int targetDivisorCount = 500;
+    long sum = 0;
+
+    for (int i = 1; true; i++) {
+      sum+= i;
+      int actualDivisors = getNumberOfDivisors(sum);
+      if (actualDivisors % 100 == 0) {
+        System.out.printf("Currently at %d divisors%n", actualDivisors);
+      }
+      if (actualDivisors >= targetDivisorCount) {
+        break;
+      }
+    }
+    return sum;
+  }
+
+  public static int getNumberOfDivisors(long n) {
+
+    int count = 0;
+    for (int i = 1; i <= Math.sqrt(n); i++) {
+      if (n % i == 0) {
+        //if it's a perfect square, then incrment divisors by 1. Else increment divisors by 2
+        count += n / i == i ? 1:2;
+      }
+    }
+    return count;
   }
 }
