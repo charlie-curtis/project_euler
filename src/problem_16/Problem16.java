763f3ea8eb9a5f1d687f843d8753a3d658faa480
Charlie Curtis <charlie-curtis@outlook.com>
2022-09-26 00:42:42 -0400

    do problem 16

---
+++ src/problem_16/Problem16.java
@@ -0,0 +1,19 @@
+package problem_16;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Problem16 {
+
+  public static void main(String[] args) {
+
+    System.out.printf("The answer is %d\n", compute());
+  }
+
+  //multiply 2^100 and sum the digits of the answer
+  public static long compute() {
+    BigInteger result = (new BigInteger("2")).pow(1000);
+    return Arrays.stream(result.toString().split("")).mapToInt(Integer::parseInt).sum();
+  }
+}
