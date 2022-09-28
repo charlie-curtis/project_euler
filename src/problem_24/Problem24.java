2be5fb1d3b2a187673ed32c7718209dc95e5c952
Charlie Curtis <charlie-curtis@outlook.com>
2022-09-28 10:18:46 -0400

    print numbers in place

--- src/problem_24/Problem24.java
+++ src/problem_24/Problem24.java
@@ -2,57 +2,68 @@ package problem_24;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 
 public class Problem24 {
 
+  private static int CUTOFF = 1_000_000;
   public static void main(String[] args) {
 
     System.out.printf("Answer is %s%n", compute());
   }
 
+  /**
+   * I solved the problem 2 ways.
+   * The first way is printing the characters in lexicographical order. This was tricky because
+   * it requires special attention to how characters are being swapped.
+   * The second way is just permutating the characters, adding them to a set, then returning
+   * the 1 millionth entry in the set.
+   * @return
+   */
   public static String compute() {
 
     String s = "0123456789";
 
     //tried 2785960341. it was wrong
-    int cutoff = 1_000_000;
     permutations = new ArrayList<>();
     printRecursive(s, 0, s.length()-1);
     permutations.sort(Comparator.naturalOrder());
 
-    return permutations.get(cutoff -1);
+    //We can either do this using a set, or print the 1M value
+    return permutations.get(CUTOFF-1);
   }
 
   private static ArrayList<String> permutations;
+  private static int count = 0;
   public static void printRecursive(String s, int startIndex, int endIndex)
   {
     if (startIndex == endIndex) {
+      //no more characters left to permutate
+      count++;
+      //System.out.println(s);
+      if (count == CUTOFF) {
+        System.out.printf("The 1M value is %s%n", s);
+      }
       permutations.add(s);
-      //System.out.printf("Answer is %s%n", s);
       return;
     }
 
-    // 0 1 2
-      //
-    // 1 0 2
-      //
-    // 2 1 0
-      //
+    String original = s;
     for (int i = startIndex; i <= endIndex; i++) {
-      s = swap(s, startIndex, i);
-      //System.out.printf("Calling recursive with i = %d and s = %s%n", startIndex+1, s);
+      s = insertCharacterForward(s, i, startIndex);
       printRecursive(s, startIndex+1, endIndex);
-      s = swap(s, startIndex, i);
-
+      s = original;
     }
   }
 
-  private static String swap(String s, int i, int j)
+  private static String insertCharacterForward(String s, int currentLocation, int desiredLocation)
   {
+    // 0 1 2 3 4 5 6 7
     char[] values = s.toCharArray();
-    values[i] = s.charAt(j);
-    values[j] = s.charAt(i);
+    char charToMove = values[currentLocation];
+    for (int i = currentLocation; i > desiredLocation; i--) {
+      values[i] = values[i-1];
+    }
+    values[desiredLocation] = charToMove;
     return String.valueOf(values);
   }
 }
