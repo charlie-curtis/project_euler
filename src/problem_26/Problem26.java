0c0fde090fd3c06fdb34a3cb9599d751aa1cc27c
Charlie Curtis <charlie-curtis@outlook.com>
2022-09-28 23:54:03 -0400

    start prob 26

---
+++ src/problem_26/Problem26.java
@@ -0,0 +1,18 @@
+package problem_26;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class Problem26 {
+
+  public static void main(String[] args) {
+
+    System.out.printf("The answer is %d\n", compute());
+  }
+
+  public static long compute() {
+    BigDecimal temp = BigDecimal.ONE;
+    BigDecimal temp2 = BigDecimal.valueOf(3);
+    return 0;
+  }
+}
