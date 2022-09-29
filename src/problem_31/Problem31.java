c54f89389d66f8306f11927fbaf766d4c0bf2a6c
Charlie Curtis <charlie-curtis@outlook.com>
2022-09-29 01:43:59 -0400

    do problem 31

---
+++ src/problem_31/Problem31.java
@@ -0,0 +1,51 @@
+package problem_31;
+
+public class Problem31 {
+
+  public static void main(String[] args) {
+
+    System.out.printf("The answer is %d\n", compute());
+  }
+
+  private static int[] denominations = {1, 2, 5, 10, 20, 50, 100, 200};
+  private static final int CUTOFF = 200;
+  private static ValueHolder[][] memo = new ValueHolder[CUTOFF+1][denominations.length];
+
+  private static class ValueHolder {
+    int value;
+    ValueHolder(int i) {value = i;}
+  }
+  public static long compute() {
+    return findCombos(CUTOFF, 0);
+  }
+
+  private static long findCombos(int amt, int idx)
+  {
+
+    if (amt == 0) {
+      return 1;
+    }
+
+    if (idx == denominations.length) {
+      return 0;
+    }
+
+    if (memo[amt][idx] != null) {
+      return memo[amt][idx].value;
+    }
+
+    int answer = 0;
+    //scenario 1 - we don't use this coin
+    answer += findCombos(amt, idx+1);
+
+    //scenario 2 - we use this coin 1 or more times
+    int tempAmt = amt;
+    while (amt - denominations[idx] >= 0) {
+      answer += findCombos(amt-denominations[idx], idx+1);
+      amt -= denominations[idx];
+    }
+
+    memo[tempAmt][idx] = new ValueHolder(answer);
+    return answer;
+  }
+}
