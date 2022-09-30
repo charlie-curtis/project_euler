799fc17b94597e87b53f22676e4c65668899ec78
Charlie Curtis <charlie-curtis@outlook.com>
2022-09-29 22:55:32 -0400

    do problem 45

---
+++ src/problem_45/Problem45.java
@@ -0,0 +1,35 @@
+package problem_45;
+
+import java.math.BigInteger;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Problem45 {
+
+  public static void main(String[] args) {
+
+    //tried 55385. it was wrong
+    //tried 15337768. it was wrong
+    //I had the right answer the first time, but I entered the INDEX of the triangle number instead of the
+    //actual triangle number (1533776805). The second time, I just copied the wrong number over
+    System.out.printf("The answer is %s\n", compute());
+  }
+
+  public static String compute() {
+    int cutoff = 1_000_000;
+    HashSet<BigInteger> s1 = new HashSet<>();
+    HashSet<BigInteger> s2 = new HashSet<>();
+    HashSet<BigInteger> s3 = new HashSet<>();
+    for (long n = 0; n < cutoff; n++) {
+      s1.add(BigInteger.valueOf(n*(n+1)/2));
+      s2.add(BigInteger.valueOf(n*(3*n-1)/2));
+      s3.add(BigInteger.valueOf(n*(2*n-1)));
+    }
+
+    return s1.stream()
+      .filter(i -> s2.contains(i) && s3.contains(i))
+      .max(Comparator.naturalOrder())
+      .get().or(BigInteger.ZERO).toString();
+  }
+}
