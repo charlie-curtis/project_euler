c6dad034dd287a305f1055a99916ccc5aa99054b
Charlie Curtis <charlie-curtis@outlook.com>
2022-09-29 23:17:43 -0400

    do problem 48

---
+++ src/problem_48/Problem48.java
@@ -0,0 +1,31 @@
+package problem_48;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class Problem48 {
+
+  public static void main(String[] args) {
+
+    //tried 6084369740. it was wrong.
+    //It was because of a long overflow. Need to be careful when dealing with really big numbers
+    System.out.printf("The answer is %s\n", compute());
+  }
+
+  public static String compute() {
+    int CUTOFF = 1000;
+    BigInteger answer = BigInteger.ZERO;
+    for (int i = 1; i <= CUTOFF; i++) {
+      BigInteger nextTerm = BigInteger.valueOf(i);
+      answer = answer.add(nextTerm.pow(i));
+    }
+
+    char[] chars = answer.toString().toCharArray();
+    char[] lastDigits = new char[10];
+    int j = chars.length -1;
+    for (int i = 9; i >= 0; j--, i--) {
+      lastDigits[i] = chars[j];
+    }
+    return String.valueOf(lastDigits);
+  }
+}
