2c46207306b36a988c39c201a0287d78616dc6eb
Charlie Curtis <charlie-curtis@outlook.com>
2022-09-29 23:52:50 -0400

    do problem 56

---
+++ src/problem_56/Problem56.java
@@ -0,0 +1,25 @@
+package problem_56;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class Problem56 {
+
+  public static void main(String[] args) {
+
+    System.out.printf("The answer is %d\n", compute());
+  }
+
+  public static long compute() {
+
+    int maxDigitalSum = Integer.MIN_VALUE;
+    for (int i = 1; i <= 100; i++) {
+      for (int j = 1; j <= 100; j++) {
+        BigInteger number = BigInteger.valueOf(i).pow(j);
+        int digitalSum = Arrays.stream(number.toString().split("")).mapToInt(Integer::parseInt).sum();
+        maxDigitalSum =Math.max(maxDigitalSum, digitalSum);
+      }
+    }
+    return maxDigitalSum;
+  }
+}
