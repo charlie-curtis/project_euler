9a60806ee45cf30d5faabe6b8ba86a3c427314b4
Charlie Curtis <charlie-curtis@outlook.com>
2022-10-04 19:03:15 -0400

    finish problem 87

--- src/problem_87/Problem87.java
+++ src/problem_87/Problem87.java
@@ -2,6 +2,10 @@ package problem_87;
 
 import helpers.PrimeCalculator;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class Problem87 {
 
   public static void main(String[] args) {
@@ -9,22 +13,36 @@ public class Problem87 {
     System.out.printf("The answer is %d\n", compute());
   }
 
+  /**
+   * I kept messing up this problem because I was counting the unique combinations of i, j, k where
+   * i,j,k were all prime and i^2 + j^3 + k^4 < 50M. In reality, I should have been tracking the numbers
+   * that satisfied this equation. I.e. I needed to dedupe the result of i^2 + j^3 + k^4.
+   */
   public static long compute() {
     int cutoff = 50_000_000;
-    int iterationCutoff = 7071; //sqrt (50M)
+    Set<Integer> answerHolder = new HashSet<>();
+    int iterationCutoff = 8000; //sqrt (50M)
     PrimeCalculator calculator = new PrimeCalculator(cutoff);
-    int count = 0;
-    for (int i = 2; i < iterationCutoff; i++) {
-      System.out.println(i);
-      for (int j = 2; j < iterationCutoff; j++) {
-        for (int k = 2; k < iterationCutoff; k++) {
-          long result = i*i + j*j*j + k*k*k*k;
-          if (calculator.isPrime(i) && calculator.isPrime(j) && calculator.isPrime(k) && (result) < cutoff) {
-            count++;
+    for (long i = 2; i < iterationCutoff; i++) {
+      if (!calculator.isPrime((int)i)) {
+        continue;
+      }
+      for (long j = 2; j < iterationCutoff; j++) {
+          if (!calculator.isPrime((int)j)) {
+            continue;
           }
+        for (long k = 2; k < iterationCutoff; k++) {
+          if (!calculator.isPrime((int)k)) {
+            continue;
+          }
+          long result = i*i + j*j*j + k*k*k*k;
+          if (result >= cutoff) {
+            break;
+          }
+          answerHolder.add((int)result);
         }
       }
     }
-    return count;
+    return answerHolder.size();
   }
 }
