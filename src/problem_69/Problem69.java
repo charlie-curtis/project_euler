2b2e0f0e9f52e8fedefaf1098d383506367433c5
Charlie Curtis <charlie-curtis@outlook.com>
2022-10-07 17:51:31 -0400

    do problem 69

---
+++ src/problem_69/Problem69.java
@@ -0,0 +1,88 @@
+package problem_69;
+
+import helpers.PrimeCalculator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * This answer is similar to question #70, so i reused the base of my code from that problem
+ */
+public class Problem69 {
+
+  public static void main(String[] args) {
+    System.out.printf("The answer is %d\n", computeFast());
+  }
+
+  private static int CUTOFF = 1_000_000;
+  //private static int CUTOFF = 79180;
+  public static long computeFast() {
+    long startTime = System.currentTimeMillis();
+    PrimeCalculator calculator = new PrimeCalculator(CUTOFF);
+    double maxRatio = Double.MIN_VALUE;
+    int bestIndex = -1;
+    for (int i = 2; i <= CUTOFF; i++) {
+      if (i % 10000 == 0) {
+        System.out.printf("%d of %d (%.2f percent)%n", i, CUTOFF, (double)i*100/CUTOFF);
+      }
+
+      int totient = getTotient(i, calculator);
+      double ratio = (double)i/totient;
+      if (ratio > maxRatio) {
+        maxRatio = ratio;
+        bestIndex = i;
+      }
+
+    }
+    long endTime = System.currentTimeMillis();
+    System.out.printf("ElapsedTime: %d%n", endTime-startTime);
+    return bestIndex;
+  }
+
+  private static HashMap<Integer, Integer> computedTotients = new HashMap<>();
+
+  /**
+   * Kinda hard to read, but there is alot of optimizations baked in to get the totient function
+   * to return quickly
+   */
+  private static int getTotient(int n, PrimeCalculator calc)
+  {
+    if (calc.isPrime(n)) {
+      computedTotients.put(n, n-1);
+      return computedTotients.get(n);
+    }
+    ArrayList<Integer> listOfPrimes = calc.getListOfPrimes();
+    HashSet<Integer> primeFactors = new HashSet<>();
+    int original = n;
+    int valueToUseInTotientEquation = n;
+    int previouslyComputedTotients = 1;
+
+    for (int j = 0; j < listOfPrimes.size(); j++) {
+      if (listOfPrimes.get(j) > n) {
+        //no more values to check. n should be 1
+        break;
+      }
+    if (computedTotients.containsKey(n)) {
+      //we can short circuit here since totient functions are multiplicative
+      valueToUseInTotientEquation /= n;
+      previouslyComputedTotients*= computedTotients.get(n);
+      break;
+    }
+      while (n % listOfPrimes.get(j) == 0) {
+        primeFactors.add(listOfPrimes.get(j));
+        n /= listOfPrimes.get(j);
+      }
+    }
+
+    double val = 1;
+    for (Integer k : primeFactors) {
+      val *= (1-(1/(double)k));
+    }
+    //we can use previously computed totients to shorten our computation
+    //so totient(n*m) = totient(n)*totient(m)
+    computedTotients.put(original, (int)(val*valueToUseInTotientEquation*previouslyComputedTotients));
+    return computedTotients.get(original);
+  }
+}
