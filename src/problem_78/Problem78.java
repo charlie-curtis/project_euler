2f66cf0f831d8a0be0e2004664c93dc5b2b792df
Charlie Curtis <charlie-curtis@outlook.com>
2022-10-07 11:13:59 -0400

    Revert "last attempt. this was too slow but passed smaller test cases. Will likely need to use the generator function if i wnat to get the right answer"
    This reverts commit 19eee685efd06a61b7cd2ac86bf62d4789cc8d81.

--- src/problem_78/Problem78.java
+++ src/problem_78/Problem78.java
@@ -2,7 +2,6 @@ package problem_78;
 
 import helpers.PrimeCalculator;
 
-import java.math.BigInteger;
 import java.util.AbstractMap;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -17,23 +16,19 @@ public class Problem78 {
     System.out.printf("The answer is %d\n", compute());
   }
 
-  //private static int CUTOFF = 20;
-  private static int CUTOFF = 60_000;
+  //private static int CUTOFF = 4000;
+  private static int CUTOFF = 10_000;
 
   private static int LOOKING_FOR = 1_000_000;
-  //private static int LOOKING_FOR = 176;
 
+  private static HashMap<AbstractMap.SimpleEntry<Integer, Integer>, Integer> memo = new HashMap<>();
   public static long compute() {
 
-    HashMap<AbstractMap.SimpleEntry<Integer, Integer>, BigInteger> map = bottomUpMap(CUTOFF);
+    long[][] map = bottomUpMap(CUTOFF);
 
     int answer = 0;
-    BigInteger modValue = BigInteger.valueOf(LOOKING_FOR);
-    BigInteger zero = BigInteger.ZERO;
-
-    for (int i = 1; i <= CUTOFF; i++) {
-      AbstractMap.SimpleEntry<Integer, Integer> entry = new AbstractMap.SimpleEntry<>(i, i);
-      if (map.get(entry).mod(modValue).equals(zero)) {
+    for (int i = 1; i < map.length; i++) {
+      if (map[i][i] % LOOKING_FOR == 0) {
        answer = i;
        break;
       }
@@ -41,43 +36,34 @@ public class Problem78 {
     return answer;
   }
 
-  private static HashMap<AbstractMap.SimpleEntry<Integer, Integer>, BigInteger> bottomUpMap(int n)
+  private static long[][] bottomUpMap(int n)
   {
     //row is n
     //col is valueToConsider
-    HashMap<AbstractMap.SimpleEntry<Integer, Integer>, BigInteger> dp = new HashMap<>();
+    long[][] dp = new long[n+1][n+1];
 
-    for (int i = 0; i <= n; i++) {
-      dp.put(new AbstractMap.SimpleEntry<>(i, 0), BigInteger.ZERO);
-    }
-
-    for (int j = 0; j<=n; j++) {
-      AbstractMap.SimpleEntry<Integer, Integer> entry = new AbstractMap.SimpleEntry<>(0, j);
-      dp.put(entry, BigInteger.ONE);
+    for (int j = 0; j< dp.length; j++) {
+      dp[0][j] = 1;
     }
 
 
     for (int i = 1; i <= n; i++) {
-      if (i % 10 == 0) {
-        System.out.printf("Computing for i = %d. That is %.4f percent%n", i, (double)i*100/CUTOFF);
-      }
       for (int j = 1; j<=n; j++) {
 
         //This will be the sum if we don't use this value at
         //all in our answer. So for example, if value = 3, and n = 10,
         //this will count all the scenarios where we add to 10 without
         //using a 3 (i.e. 5+2+1+1+1 is one such example)
-        BigInteger sum = dp.get(new AbstractMap.SimpleEntry<>(i, j-1));
+        long sum = dp[i][j-1];
 
         //if we use value once, it looks like
         //dp[n-i][j-1]
         //if we use it twice, it looks like dp[n-2*i][j-1]
         for (int k = 1; k*j <= i; k++) {
-          AbstractMap.SimpleEntry<Integer, Integer> entry = new AbstractMap.SimpleEntry<>(i -k*j, j-1);
-          sum = sum.add(dp.get(entry));
+          sum += dp[i-k*j][j-1];
         }
 
-        dp.put(new AbstractMap.SimpleEntry<>(i,j), sum);
+        dp[i][j] = sum;
       }
     }
     return dp;
