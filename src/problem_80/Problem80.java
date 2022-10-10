ec3b6a9a3b7411fba91ce34f03412ea8231ee435
Charlie Curtis <charlie-curtis@outlook.com>
2022-10-10 18:38:12 -0400

    do problem 80

--- src/problem_80/Problem80.java
+++ src/problem_80/Problem80.java
@@ -1,8 +1,9 @@
 package problem_80;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.math.MathContext;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Problem80 {
 
@@ -13,23 +14,17 @@ public class Problem80 {
 
   public static long compute() {
 
-    long sum = 0;
-    for (int i = 2; i <= 100; i++) {
-      if (Math.sqrt(i) % 1 == 0) {
-        continue;
-      }
-      MathContext context = new MathContext(115);
-      String result = BigDecimal.valueOf(i).sqrt(context).toString();
-      int j = 0;
-      while (j < 101) {
-        if (result.charAt(j) == '.') {
-          j++;
-          continue;
-        }
-        sum+= Integer.parseInt(result.substring(j, j+1));
-        j++;
-      }
-    }
-    return sum;
+    return IntStream.range(1, 101) //first 100 natural numbers
+        .filter(num -> Math.sqrt(num) % 1 != 0) //filter out perfect squares:w
+        .mapToObj(num -> {
+          //get sqrt with 100 decimal digits -- include some extra padding
+          MathContext context = new MathContext(115);
+          return BigDecimal.valueOf(num).sqrt(context).toString();
+        })
+        .map(s1 -> s1.replace('.', '0')) //get rid of the decimal point
+        //convert the string into an array, take the first 100 numbers -- 1 extra for the decimal point we replaced
+        .flatMap(s1 -> Arrays.stream(s1.split("")).limit(101))
+        //cast & sum
+        .mapToLong(Long::parseLong).sum();
   }
 }
